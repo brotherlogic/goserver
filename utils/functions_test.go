d3d8e61c0e15e88e185aa3555a1bd636b5bbe084
Simon Tucker <brotherlogic@gmail.com>
2017-12-13 06:12:24 -0800

    Fixes fuzzy match

--- utils/functions_test.go
+++ utils/functions_test.go
@@ -3,13 +3,24 @@ package utils
 import (
 	"testing"
 
+	pbgd "github.com/brotherlogic/godiscogs"
 	pb "github.com/brotherlogic/goserver/proto"
+	pbrc "github.com/brotherlogic/recordcollection/proto"
 )
 
 func TestEmbed(t *testing.T) {
 	a := &pb.EmbeddedTest{Blah: &pb.Alive{Name: "blah"}}
 	if !FuzzyMatch(a, a) {
 		t.Errorf("Failure to match on embedded: %v", a)
+	}
+}
+
+func TestFuzzyMatchDetailed(t *testing.T) {
+	a := &pbrc.Record{Release: &pbgd.Release{Id: 1234, FolderId: 1, Title: "Bonkers"}}
+	b := &pbrc.Record{Release: &pbgd.Release{FolderId: 1}}
+
+	if !FuzzyMatch(b, a) {
+		t.Errorf("Failed to match on detailed: %v != %v", b, a)
 	}
 }
 
