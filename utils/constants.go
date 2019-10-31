cde301af66e256bf889e8e5ad556819f444950c4
Simon Tucker <simon@brotherlogic@gmail>
2019-10-30 19:46:05 -0700

    Changes

--- utils/constants.go
+++ utils/constants.go
@@ -2,11 +2,11 @@ package utils
 
 const (
 	// RegistryIP - Local IP for discover
-	RegistryIP = "192.168.86.45"
+	RegistryIP = "192.168.86.40"
 
 	// RegistryPort - port for discover
 	RegistryPort = 50055
 
 	// Discover the overall thing for discover
-	Discover = "192.168.86.45:50055"
+	Discover = "192.168.86.40:50055"
 )
