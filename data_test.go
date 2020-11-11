97b88363f32526d0073a439da209665d74245fee
Simon Tucker <brotherlogic@gmail.com>
2020-11-10 16:13:00 -0800

    Defines a translation layer. This closes #582

---
+++ data_test.go
@@ -0,0 +1,30 @@
+package goserver
+
+import (
+	"context"
+	"testing"
+
+	pb "github.com/brotherlogic/goserver/proto"
+	google_protobuf "github.com/golang/protobuf/ptypes/any"
+)
+
+func TestBasicStore(t *testing.T) {
+	mts := &mts{store: &memstore{mem: make(map[string]*google_protobuf.Any)}}
+
+	test := &pb.Alive{Name: "testing"}
+
+	err := mts.save(context.Background(), "test", test)
+	if err != nil {
+		t.Fatalf("Unable to save: %v", err)
+	}
+
+	t2 := &pb.Alive{}
+	err = mts.load(context.Background(), "test", t2)
+	if err != nil {
+		t.Fatalf("Unable to load: %v", err)
+	}
+
+	if t2.GetName() != "testing" {
+		t.Errorf("Translation has failed: %v", t2)
+	}
+}
