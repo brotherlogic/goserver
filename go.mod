a1431cac822c48ec4e629a533e9a5fe222d85d73
simon <brotherlogic@gmail.com>
2021-07-28 10:28:08 -0700

    Add a count of startup routines

--- go.mod
+++ go.mod
@@ -3,27 +3,28 @@ module github.com/brotherlogic/goserver
 go 1.15
 
 require (
-	github.com/brotherlogic/buildserver v0.0.0-20201019183109-5b9d0c13b530
-	github.com/brotherlogic/datastore v0.0.0-20201116152557-1a9915463b93
-	github.com/brotherlogic/discovery v0.0.0-20200925011539-74a8dc203ba4
-	github.com/brotherlogic/githubcard v0.0.0-20201205192049-f6e63a2a7d72
-	github.com/brotherlogic/gobuildslave v0.0.0-20201112143137-ce3a2e75a18e
+	github.com/brotherlogic/buildserver v0.0.0-20210616011633-2d28fd61c61d
+	github.com/brotherlogic/datastore v0.0.0-20210713131041-be3230cc69b8
+	github.com/brotherlogic/discovery v0.0.0-20210626180714-1ae56396f4e1
+	github.com/brotherlogic/githubcard v0.0.0-20210608134457-caf114866b5e
+	github.com/brotherlogic/gobuildslave v0.0.0-20210626211527-ff107a8a9b1d
 	github.com/brotherlogic/godiscogs v0.0.0-20201012230422-e6d22566e955
 	github.com/brotherlogic/keystore v0.0.0-20201029130503-da423c07fd79
-	github.com/brotherlogic/logging v0.0.0-20200903150238-67a07e215efe
+	github.com/brotherlogic/lock v0.0.0-20210715133423-34193158ab11
+	github.com/brotherlogic/logging v0.0.0-20210611234942-a3917f7f53cf
 	github.com/brotherlogic/monitor v0.0.0-20200610021023-f01db8d3fdc3
 	github.com/brotherlogic/recordcollection v0.0.0-20201126210339-d58e20a26a3b
 	github.com/brotherlogic/recordgetter v0.0.0-20201227170856-8696381a15a9 // indirect
 	github.com/brotherlogic/tracer v0.0.0-20200519014902-f0edd3e0722a
 	github.com/brotherlogic/versionserver v0.0.0-20200424225322-e02615d8678f
-	github.com/golang/protobuf v1.4.3
+	github.com/golang/protobuf v1.5.2
 	github.com/mitchellh/go-ps v1.0.0
-	github.com/prometheus/client_golang v1.9.0
-	github.com/struCoder/pidusage v0.1.3
-	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
-	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
-	golang.org/x/text v0.3.4 // indirect
-	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
-	google.golang.org/grpc v1.34.0
-	google.golang.org/protobuf v1.25.0
+	github.com/prometheus/client_golang v1.11.0
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.7.1 // indirect
+	github.com/struCoder/pidusage v0.2.0
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985
+	google.golang.org/genproto v0.0.0-20210726200206-e7812ac95cc0 // indirect
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
 )
--- goserverapi.go
+++ goserverapi.go
@@ -1,7 +1,6 @@
 package goserver
 
 import (
-	"bufio"
 	"context"
 	"errors"
 	"fmt"
@@ -10,7 +9,6 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/exec"
 	"runtime"
 	"runtime/debug"
 	"runtime/pprof"
@@ -70,6 +68,11 @@ var (
 	serverPeak = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "rpc_server_peak_requests",
 		Help: "The number of server requests",
+	})
+
+	startupRoutines = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "startup_routines",
+		Help: "The number of running routines at startup",
 	})
 
 	clientRequests = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -1270,6 +1273,9 @@ func (s *GoServer) Serve(opt ...grpc.ServerOption) error {
 
 	s.startup = time.Now()
 
+	// Count the number of active routines here
+	startupRoutines.Set(float64(runtime.NumGoroutine()))
+
 	server.Serve(lis)
 	return nil
 }
@@ -1466,104 +1472,4 @@ func (s *GoServer) registerServer(IP string, servername string, external bool, v
 	s.Registry = r.GetService()
 
 	return r.GetService().Port, nil
-}
-
-var election = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "server_election",
-	Help: "The number of active election waits",
-})
-
-func (s *GoServer) runElection(key string, elected chan error, complete chan bool) {
-	if s.SkipElect {
-		elected <- nil
-		return
-	}
-	election.Inc()
-	defer election.Dec()
-	command := exec.Command("etcdctl", "elect", s.Registry.Name+key, s.Registry.Identifier)
-	command.Env = append(os.Environ(),
-		"ETCDCTL_API=3",
-	)
-	out, _ := command.StdoutPipe()
-	defer out.Close()
-	if out != nil {
-		scanner := bufio.NewScanner(out)
-		go func() {
-			for scanner != nil && scanner.Scan() {
-				text := scanner.Text()
-				// Expect something like registry.name/key
-				if strings.HasPrefix(text, s.Registry.Name) {
-					elected <- nil
-				} else {
-					elected <- fmt.Errorf("Unable to elect (%v): %v", s.Registry.Name+key, scanner.Text())
-				}
-			}
-		}()
-	}
-
-	out2, _ := command.StderrPipe()
-	defer out2.Close()
-	if out2 != nil {
-		scanner := bufio.NewScanner(out2)
-		go func() {
-			for scanner != nil && scanner.Scan() {
-				text := scanner.Text()
-				s.Log(fmt.Sprintf("ERR run %v", text))
-				if strings.HasPrefix(text, s.Registry.Name) {
-					elected <- nil
-				} else {
-					elected <- fmt.Errorf("Unable to from err elect (%v): %v", s.Registry.Name+key, scanner.Text())
-				}
-			}
-		}()
-	}
-
-	//Run the election
-	err := command.Start()
-	if err != nil {
-		s.Log(fmt.Sprintf("Error starting command: %v", err))
-	}
-	<-complete
-	command.Process.Kill()
-
-	// Ensure that the command is stopped an removed
-	command.Wait()
-}
-
-//Elect elect me
-func (s *GoServer) Elect() (func(), error) {
-	if s.SkipElect {
-		return func() {
-
-		}, nil
-	}
-	elected := make(chan error, 1)
-	complete := make(chan bool, 1)
-	rf := func() {
-		complete <- true
-	}
-
-	go s.runElection("", elected, complete)
-
-	err := <-elected
-	return rf, err
-}
-
-//ElectKey elect me with a key
-func (s *GoServer) ElectKey(key string) (func(), error) {
-	if s.SkipElect {
-		return func() {
-
-		}, nil
-	}
-	elected := make(chan error)
-	complete := make(chan bool)
-	rf := func() {
-		complete <- true
-	}
-
-	go s.runElection(key, elected, complete)
-
-	err := <-elected
-	return rf, err
 }
